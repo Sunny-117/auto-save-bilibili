e19f9358d40f30932ab232654429df5109545fc0
付志强 <zhiqiangfu6@gmail.com>
2022-11-06 23:21:13 +0800

    feat: add new feature

--- index.js
+++ index.js
@@ -2,9 +2,9 @@
 const getList = async () => {
     const res = await fetch('https://api.bilibili.com/x/space/arc/search?mid=12890453&ps=30&tid=0&pn=2&keyword=&order=pubdate&order_avoided=true&jsonp=jsonp')
     const d = await res.json()
-    console.log(d)
     return d
 }
-const e = getList()
-console.log(e)
+getList().then(res => {
+    console.log(res.data.list.vlist)
+})
 
