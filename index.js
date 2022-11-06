7e54a3b02a68be9524497df437d82d2ee221b74a
付志强 <zhiqiangfu6@gmail.com>
2022-11-06 23:16:13 +0800

    feat: add new feature

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-import axios from 'axios'
-console.log(axios)
+import fetch from 'node-fetch'
+console.log(fetch)
 let pn = 1
-const getList = async () => {
-    const { data } = await axios.get(`https://api.bilibili.com/x/space/arc/search?mid=12890453&ps=30&tid=0&pn=1&keyword=&order=pubdate&order_avoided=true&jsonp=jsonp`)
-    console.log(data)
-}
-getList()
+// const getList = async () => {
+
+//     const { data } = await axios.get(`https://api.bilibili.com/x/space/arc/search?mid=12890453&ps=30&tid=0&pn=1&keyword=&order=pubdate&order_avoided=true&jsonp=jsonp`)
+//     console.log(data)
+// }
+// getList()
