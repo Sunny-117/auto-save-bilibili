781fa3e5a526d5311efcf6b5dcc39cf28a8fdd34
付志强 <zhiqiangfu6@gmail.com>
2022-11-06 23:17:53 +0800

    feat: add new feature

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import fetch from 'node-fetch'
 let pn = 1
 const getList = async () => {
     const data = await fetch('https://api.bilibili.com/x/space/arc/search?mid=12890453&ps=30&tid=0&pn=2&keyword=&order=pubdate&order_avoided=true&jsonp=jsonp')
-    console.log(data)
+    const res = await data.json()
+    console.log(res)
 }
 getList()
