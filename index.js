e2b93e5597134736e716ffdc4a16f883314a8e37
付志强 <zhiqiangfu6@gmail.com>
2022-11-06 23:23:43 +0800

    feat: add new feature

--- index.js
+++ index.js
@@ -10,3 +10,22 @@ getList().then(res => {
     }
 })
 
+
+
+function downFlie(jsonObj) {
+    // 创建a标签
+    var elementA = document.createElement('a');
+
+    //文件的名称为时间戳加文件名后缀
+    elementA.download = +new Date() + ".json";
+    elementA.style.display = 'none';
+
+    //生成一个blob二进制数据，内容为json数据
+    var blob = new Blob([JSON.stringify(jsonObj)]);
+
+    //生成一个指向blob的URL地址，并赋值给a标签的href属性
+    elementA.href = URL.createObjectURL(blob);
+    document.body.appendChild(elementA);
+    elementA.click();
+    document.body.removeChild(elementA);
+}
